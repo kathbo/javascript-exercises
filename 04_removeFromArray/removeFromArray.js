87fd34dd193213f122cddf4c3ab58fe03cb341ee
berry <kathborowy@gmail.com>
2022-06-03 17:24:41 +0200

    1st slution, takes multiple values

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,13 @@
-const removeFromArray = function(arr, n) {
-    return arr.filter((el) => {
-      if (el !== n) {
-        return el
-      }})
-    };
+const removeFromArray = function(arr, ...n) {
+    for (let el of n) { // 2 and 4
+      for (let x = 0; x < arr.length; x++) {
+        if (arr[x] === el) {
+          arr.splice(x, 1)
+        }
+      }
+    }
+    return arr
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
