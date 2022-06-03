8391ce5f89278d38eb145610f21db80c1f89bf75
berry <kathborowy@gmail.com>
2022-06-03 15:29:36 +0200

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
-};
+const reverseString = function(str) {
+    let arr = str.split(''); // str -> arr
+    arr.reverse();
+    return arr.join('');
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
