f622ac0dff80c4f44c64a8773c604bd23640bbfe
berry <kathborowy@gmail.com>
2022-06-03 17:21:40 +0200

    1st slution, takes onlu one value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, n) {
+    return arr.filter((el) => {
+      if (el !== n) {
+        return el
+      }})
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
