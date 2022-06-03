dcecaf0e5b713a0abf4c46835e52060d5c61d3ad
berry <kathborowy@gmail.com>
2022-06-03 17:52:14 +0200

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    let bigger = Math.max(a,b);
+    let smaller = Math.min(a,b);
+    let sum = 0;
+    for (let x = smaller; x <= bigger; x++) {
+      sum += x;
+    }
+    return (a < 0 || b < 0 || typeof(a) != 'number' || typeof(b) != 'number') ? "ERROR" : sum;
+  };
+  
 
 // Do not edit below this line
 module.exports = sumAll;
